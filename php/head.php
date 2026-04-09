b5f0547907ed1f9779ddcd0e2e0e31026a8170e8
NotGedz <nazar.khristiyanin@gmail.com>
2026-04-09 11:19:31 +0200

    revision 1

--- php/head.php
+++ php/head.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SOTS Modpack</title>
     <link rel="icon" type="image/x-icon" href="img/sots.ico">
-    <link rel="stylesheet" href="./css/output.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="/sots/css/output.css">
+    <link rel="stylesheet" href="/sots/css/style.css">
     <link rel="stylesheet" href="https://unpkg.com/98.css">
 </head>
